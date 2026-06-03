b8975293c9bb4dc6a8d04d73cee833eeed73ff9c
raksh62 <rdasa@student.unimelb.edu.au>
2026-06-03 22:14:05 +1000

    wrote my first checkout javascript for the order

--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,9 @@ const searchBar = document.getElementById("searchBar")
 searchBar.addEventListener("input", function() {
     console.log(searchBar.value );
 });
+
+const padThaiPrice = 22;
+const greenCurryPrice =25;
+
+let orderTotal=padThaiPrice+ greenCurryPrice;
+console.log (orderTotal);
