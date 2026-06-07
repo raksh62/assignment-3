e66ee33a23539ad17189a55e0497d29496865fba
raksh62 <rdasa@student.unimelb.edu.au>
2026-06-08 01:15:30 +1000

    defined all of my objects within the java script code for description image and price

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,36 @@
 let order = {
+    padThai: 0,
     greenCurry: 0,
-    padThai: 0
+    padSee: 0,
+    redCurry:0,
     
 };
 
 let price = {
- greenCurry: 22,
  padThai: 25,
+ greenCurry: 22,
  padSee: 23,
  redCurry:20,
 };
+
+let description = {
+    padThai:"Stir fried rice noodles with  egg, bean sprouts, spring onion and crushed peanuts, tossed in a tamarind sauce. Served with lime.",
+    greenCurry: "Traditional Thai green curry with coconut milk and seasonal vegetables. Served with Jasmine Rice.",
+    padSee: "Stir fried wide noodles with egg, chinese broccoli and your choice of protein, tossed in a rich sauce. ",
+    redCurry: "Traditional Thai style curry simmered in coconut milk. Served with Jasmine Rice. "
+}
+
+const dishCards = document.querySelectorAll(".dish-card");
+
+dishCards.forEach(function(card) {
+
+    card.addEventListener("click", function() {
+
+        document.getElementById("dishPanel").style.display = "block";
+
+    });
+
+});
 
 function increaseQty(item) {
     order[item]++;
